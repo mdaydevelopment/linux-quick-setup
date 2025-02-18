8c919b9e1c08734959cb1f3842b65edf00f858a8
Michael Day <day.michael.b@gmail.com>
2025-02-18 15:45:04 -0600

    conditional prompt symbols

--- bashit/mday_pete/mday_pete.theme.bash
+++ bashit/mday_pete/mday_pete.theme.bash
@@ -7,10 +7,28 @@ function prompt_setter() {
     scm_char="$(scm_char)"
     scm_prompt_info="$(scm_prompt_info)"
     k8s_context_prompt="$(k8s_context_prompt)"
-    wheel_code='\U2388'
-    wheel="$(echo -e $wheel_code)"
+    function k8s_prompt() {
+        wheel_code='\U2388'
+        wheel="$(echo -e $wheel_code)"
+        if [[ -n "${k8s_context_prompt:-}" ]]; then
+            echo -ne "${wheel}${k8s_context_prompt}"
+        else
+            echo -ne ""
+        fi
+    }
+    k8s_context_prompt="$(k8s_prompt)"
     virtualenv_prompt="$(virtualenv_prompt)"
     aws2_profile="$(aws2_profile)"
+    function aws2_prompt() {
+        cloud_symbol='\U00002601'
+        cloud=$(echo -e ${cloud_symbol})
+        if [[ $(aws2_profile) != "default" ]]; then
+            echo -ne "${cloud}${AWS_PROFILE}"
+        else
+            echo -ne ""
+        fi
+    }
+    aws2_profile="$(aws2_prompt)"
     _save-and-reload-history 1 # Save history
     PS1="(${clock_prompt}) ${scm_char} [${blue?}\u${reset_color?}@${green?}\H${reset_color?}] ${yellow?}\w${reset_color?}${scm_prompt_info} ${blue?}${wheel}${k8s_context_prompt}${reset_color?} ${green?}${virtualenv_prompt} ${yellow?}${aws2_profile} ${reset_color?} "
     PS1+="\n${green?}→${reset_color?} "
--- lqs_bashrc.sh
+++ lqs_bashrc.sh
@@ -7,11 +7,9 @@ eval "$(thefuck --alias)"
 
 # aws cli v2 prompt helper
 function aws2_profile() {
-    cloud_symbol='\U00002601'
-    cloud=$(echo -e ${cloud_symbol})
     if [[ -n "${AWS_PROFILE:-}" ]]; then
         echo -ne "${cloud}${AWS_PROFILE}"
     else
-        echo -ne ""
+        echo -ne "default"
     fi
 }
