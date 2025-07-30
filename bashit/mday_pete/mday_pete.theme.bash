b5ff6a2739d7fb28348b17ac762171787e91c78b
mday <day.michael.b@gmail.com>
2025-07-30 16:04:48 -0500

    zulu time in prompt

--- bashit/mday_pete/mday_pete.theme.bash
+++ bashit/mday_pete/mday_pete.theme.bash
@@ -4,6 +4,18 @@
 function prompt_setter() {
     local clock_prompt scm_char scm_prompt_info ruby_version_prompt
     clock_prompt="$(clock_prompt)"
+    function utc_clock_prompt() {
+        local clock_color="${THEME_CLOCK_COLOR:-${normal?}}"
+        local clock_format="${THEME_CLOCK_FORMAT:-"%H:%M:%S"}"
+        local show_clock="${THEME_SHOW_CLOCK:-${THEME_CLOCK_CHECK:-true}}"
+        #local clock_string="$(TZ='Etc/UTC' date +%H:%M:%S)"
+        local clock_string="$(TZ='Etc/UTC' date +$clock_format)"
+
+        if [[ "${show_clock}" == "true" ]]; then
+            echo -ne "${clock_color}${CLOCK_THEME_PROMPT_PREFIX-}${clock_string}${CLOCK_THEME_PROMPT_SUFFIX-}"
+        fi
+    }
+    utc_clock_prompt="$(utc_clock_prompt)"
     scm_char="$(scm_char)"
     scm_prompt_info="$(scm_prompt_info)"
     k8s_context_prompt="$(k8s_context_prompt)"
@@ -38,7 +50,7 @@ function prompt_setter() {
         fi
     }
     _save-and-reload-history 1 # Save history
-    PS1="(${clock_prompt}) ${scm_char} [${blue?}\u${reset_color?}@${green?}\H${reset_color?}] ${yellow?}\w${reset_color?}"
+    PS1="L(${clock_prompt})Z(${utc_clock_prompt}) ${scm_char} [${blue?}\u${reset_color?}@${green?}\H${reset_color?}] ${yellow?}\w${reset_color?}"
     context_line
     PS1+="${scm_prompt_info}${blue?}${wheel}${k8s_context_prompt}${reset_color?}${green?}${virtualenv_prompt}${yellow?}${aws_profile}${reset_color?} "
     PS1+="\n${green?}→${reset_color?} "
